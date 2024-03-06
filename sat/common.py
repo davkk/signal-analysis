662e360ebd88d2214763c707db5c4f8922c33134
davkk <dadulerk@gmail.com>
2024-03-06 17:23:21 +0100

    feat: project restructure

---
+++ sat/common.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+
+
+def set_custom_pyplot_styles():
+    # custom pyplot styles
+    SMALL_SIZE = 14
+    MEDIUM_SIZE = 16
+    BIGGER_SIZE = 22
+
+    plt.rcParams["figure.figsize"] = (9, 8)
+    # plt.rcParams["figure.dpi"] = 200
+    plt.rcParams["font.family"] = "serif"
+    plt.rcParams["mathtext.fontset"] = "stix"
+    plt.rcParams["xtick.direction"] = "in"
+    plt.rcParams["ytick.direction"] = "in"
+
+    plt.rcParams["axes.formatter.limits"] = -3, 3
+    plt.rcParams["axes.grid"] = False
+    plt.rcParams["grid.color"] = "gainsboro"
+    plt.rcParams["axes.formatter.use_mathtext"] = True
+
+    plt.rc("font", size=SMALL_SIZE)  # controls default text sizes
+    plt.rc("axes", titlesize=SMALL_SIZE)  # fontsize of the axes title
+    plt.rc("axes", labelsize=MEDIUM_SIZE)  # fontsize of the x and y labels
+    plt.rc("xtick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
+    plt.rc("ytick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
+    plt.rc("legend", fontsize=SMALL_SIZE)  # legend fontsize
+    plt.rc("figure", titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
+    colors = plt.rcParams["axes.prop_cycle"].by_key()["color"]
+    markers = ["*", "1", "+", "2", ".", "3"]
+    return colors, markers
---
+++ sat/lab01/fourier.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from sat.common import set_custom_pyplot_styles
+from sat.lab01.wave import sample_rate, wave
+
+spectrum = np.fft.fft(wave)
+freqs = np.fft.fftfreq(wave.size, 1 / sample_rate)
+
+f_range = (freqs >= 16) & (freqs <= 4e3)
+freqs = freqs[f_range]
+spectrum = spectrum[f_range]
+
+spectrum_db = 10 * np.log10(np.abs(spectrum) + 1e-15)
+
+if __name__ == "__main__":
+    set_custom_pyplot_styles()
+
+    plt.loglog(freqs, spectrum_db)
+
+    plt.title("FFT spectrum")
+    plt.xlabel(r"Frequency $f$ [Hz]")
+    plt.ylabel(r"Power $X(f)$ [dB]")
+
+    plt.tight_layout()
+    plt.savefig(Path(__file__).with_suffix(".pdf"))
+    # plt.show()
